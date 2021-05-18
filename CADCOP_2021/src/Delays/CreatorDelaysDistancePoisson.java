5ad7b580024d77c481c8d68d49bb16d6defc3b2a
benrachmut <rachmut.ben@gmail.com>
2021-05-18 16:01:06 +0300

    blaaaaa

---
+++ CADCOP_2021/src/Delays/CreatorDelaysDistancePoisson.java
@@ -0,0 +1,35 @@
+package Delays;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CreatorDelaysDistancePoisson extends CreatorDelaysMatrixDependenet{
+	private double[] multipliers= {500,1000,1500,2000,5000};
+
+ 
+	@Override
+	protected ProtocolDelay createDefultProtocol(double gamma) {
+		 return new ProtocolDelaysDistancePoisson(gamma);
+	}
+
+	@Override
+	protected Collection<? extends ProtocolDelay> createCombinationsDelay(boolean isTimeStamp, double gamma) {
+		List<ProtocolDelay> ans = new ArrayList<ProtocolDelay>();
+		for (double m : multipliers) {
+			ans.add(new ProtocolDelaysDistancePoisson(isTimeStamp, gamma, m));
+		} // sigma
+		return ans;
+	}
+
+	@Override
+	protected String header() {
+		return "multiplier";
+	}
+
+	@Override
+	public String name() {
+		return "Distance Poisson";
+	}
+
+}
